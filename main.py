f46d4eb249ada246221e4d89c735d267a2ed0157
Johannes Müller <johannes.mueller2000@gmail.com>
2021-07-29 17:25:55 +0200

    tech demo: added websocket connector and template engine processing

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from tornado.options import define, options
 from tornado.web import Application
 from tornado.web import RequestHandler
 from tornado.web import StaticFileHandler
-import os
+from tornado.websocket import WebSocketHandler
+from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
+import os, time, json
+
 
 define("port", default=8888, help="port to listen on")
 
@@ -14,6 +18,38 @@ class StartPage(RequestHandler):
     def get(self):
         self.render("assets/html/index.html")
 
+#Beispiel Websocket connector für websocket verbindung zur Webseite
+class WebSocketConnector(WebSocketHandler):
+    def open(self):
+        print("New client connected")
+        self.write_message("You are connected")
+
+
+    def on_message(self, message):
+        print("Message ist:", message)
+
+        try:
+            message = json.loads(message)
+            print(type(message))
+            
+            #template laden aus datei
+            file_loader = FileSystemLoader('assets/templates')
+            env = Environment(loader=file_loader)
+            template = env.get_template('test.txt')
+
+            #template mit inhalt füllen
+            output = template.render(lib2=message["content"], text1=message["content"])
+            print(output)
+
+        except Exception as e:
+            pass
+
+        print("received message {}".format(message))
+        self.write_message("You said {}".format(message))
+        self.last = time.time()
+    
+    def on_close(self):
+        print("connection is closed")
 
 
 
@@ -26,6 +62,7 @@ def main():
 
     app = Application([
         (r"/index.html", StartPage),
+        (r"/server", WebSocketConnector)
     ], **settings)
 
 
@@ -36,4 +73,6 @@ def main():
     IOLoop.current().start()
 
 main()
+
+
 
