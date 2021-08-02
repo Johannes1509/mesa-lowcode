2ece4737458f07aa27d94517367337a05bb0c9a5
Johannes Müller <johannes.mueller@ser.de>
2021-08-02 16:32:36 +0200

    tech demo: added support for adding agents and agents properties

--- assets/js/agent-types.js
+++ assets/js/agent-types.js
@@ -1,18 +1,22 @@
 class AgentTypesController{
     constructor(){
         this.agentTypes = []
-        
+        this.rowCount = 0
     }
 
     addAgentType(){
         let templateAgentType = $(".agent-type-template").clone()
-        $(templateAgentType).removeClass("d-none")
-        $(templateAgentType).removeClass("agent-type-template")
-        $("#agent-types-container").append($(templateAgentType))
+        templateAgentType = $(templateAgentType).removeClass("d-none")
+        templateAgentType = $(templateAgentType).removeClass("agent-type-template")
+        $("#add-agent-button").before($(templateAgentType))
+        this.__addProperty($(templateAgentType).find("a.btn"))
     }
 
-    __addProperty(){
-
+    __addProperty(element){
+        let templateAgentTypeProperty = $(".agent-type-template-properties").clone()
+        templateAgentTypeProperty = $(templateAgentTypeProperty).removeClass("d-none")
+        templateAgentTypeProperty = templateAgentTypeProperty.removeClass("agent-type-template-properties")
+        $(element).prev().append(templateAgentTypeProperty)
     }
 
     getAgentTypesInfo(){
