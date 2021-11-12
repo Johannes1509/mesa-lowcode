254ce99bd4e8b1ce22b946550edb97db8e9438b5
Johannes Müller <johannes.mueller2000@gmail.com>
2021-11-12 15:15:49 +0100

    integrated drawflow on early step

--- assets/js/phases/model-process.js
+++ assets/js/phases/model-process.js
@@ -4,7 +4,34 @@ class ModelProcessController extends AbstractPhase{
     }
 
     startPhase(phaseData){
-        flowy(document.getElementById("flowycanvas"), main.process.__flowyOnGrab, main.process.__flowyOnRelease, main.process.__flowyOnSnap, main.process.__flowyOnRearrange, 20, 50)
+       // flowy(document.getElementById("flowycanvas"), main.process.__flowyOnGrab, main.process.__flowyOnRelease, main.process.__flowyOnSnap, main.process.__flowyOnRearrange, 20, 50)
+       
+
+       $(function() {
+
+        $("#process-canvas-container").mousewheel(function(event, delta) {
+     
+           this.scrollLeft -= (delta * 90);
+         
+           event.preventDefault();
+     
+        });
+     
+     });
+       
+       var id = document.getElementById("process-canvas");
+       const editor = new Drawflow(id);
+       editor.start();
+            var html = `
+        <div>asdasdasdasdasdsad<input type="text" df-name></div>
+        `;
+        var data = { "name": '' };
+
+        editor.addNode('start', 0, 1, 150, 300, 'start-element', {}, $("#agent-process-start-element").html())
+        editor.addNode('end', 1, 0, 600, 300, 'start-element', {}, $("#agent-process-end-element").html())
+        editor.addNode('block', 1, 1, 300, 300, 'start-element', {}, $("#agent-process-block-element").html())
+     
+
     }
 
     stopPhase(){
--- resultfiles/agents/.py
+++ resultfiles/agents/.py
@@ -2,14 +2,11 @@ from mesa import Agent
 import random
 from random import randrage, randint, choice
 
-class TestAgent2(Agent):
+class (Agent):
     def __init__(self, uniqueId, model):
         super().__init__(uniqueId, model)
         
-        self.asdf = asdf
-        self.asdf = asdf
-        self.asdf = asdf
-        self.asdf = asdf
+        self. = 
 
     def step(self):
         
