da0eb46ab2d4080ace1c3b23d2a59b15834c4fb7
Johannes Müller <johannes.mueller2000@gmail.com>
2021-11-14 14:38:51 +0100

    second version of integration drawflow in lowcode mesa

--- assets/js/phases/model-process.js
+++ assets/js/phases/model-process.js
@@ -1,62 +1,80 @@
 class ModelProcessController extends AbstractPhase{
     init(){
         this.phaseName = "process"
+        this.canvas = new Drawflow($("#process-canvas")[0]);
+        this.canvas.start()
+        this.canvas.addModule("dummy")
+        this.canvas.changeModule("dummy")
+
+        $(function() {
+            $("#process-canvas-container").mousewheel(function(event, delta) {
+               this.scrollLeft -= (delta * 90);
+               event.preventDefault();
+            });
+        });
     }
 
-    startPhase(phaseData){
-       // flowy(document.getElementById("flowycanvas"), main.process.__flowyOnGrab, main.process.__flowyOnRelease, main.process.__flowyOnSnap, main.process.__flowyOnRearrange, 20, 50)
-       
+    startPhase(phaseData){      
+        for(var i = 0; i < phaseData["agents"].length; i++){
+            let currentAgentId = phaseData["agents"][i]["id"]
+            let currentAgentTypeElement = $("#agent-type-element-process").clone()
+            
+            currentAgentTypeElement.children().text(phaseData["agents"][i]["name"])
+            currentAgentTypeElement.children().attr("agent-type-id", currentAgentId)
+            $('#model-process-agent-types-select').append(currentAgentTypeElement)
+        }
 
-       $(function() {
+        if(phaseData["process"] == undefined){
+            main.data["process"] = {}
 
-        $("#process-canvas-container").mousewheel(function(event, delta) {
-     
-           this.scrollLeft -= (delta * 90);
-         
-           event.preventDefault();
-     
-        });
-     
-     });
-       
-       var id = document.getElementById("process-canvas");
-       const editor = new Drawflow(id);
-       editor.start();
-            var html = `
-        <div>asdasdasdasdasdsad<input type="text" df-name></div>
-        `;
-        var data = { "name": '' };
+            for(var i = 0; i < phaseData["agents"].length; i++){
+                let currentAgentId = phaseData["agents"][i]["id"]
+                this.canvas.addModule("agent-"+currentAgentId)
+                this.canvas.changeModule("agent-"+currentAgentId)
 
-        editor.addNode('start', 0, 1, 150, 300, 'start-element', {}, $("#agent-process-start-element").html())
-        editor.addNode('end', 1, 0, 600, 300, 'start-element', {}, $("#agent-process-end-element").html())
-        editor.addNode('block', 1, 1, 300, 300, 'start-element', {}, $("#agent-process-block-element").html())
-     
-
+                this.canvas.addNode('start', 0, 1, 150, 300, 'start-element', {}, $("#agent-process-start-element").html())
+                this.canvas.addNode('end', 1, 0, 600, 300, 'start-element', {}, $("#agent-process-end-element").html())          
+            }
+        }else{
+            this.canvas.import(phaseData["process"]["drawflowExport"])
+        }
+        
+    
+        this.canvas.changeModule("dummy")
     }
 
     stopPhase(){
-        
+        $('#model-process-agent-types-select').empty()
     }
 
     getJSONData(){
+        let processData = {}
+        let exportData = this.canvas.export()
+        processData["drawflowExport"] = exportData
 
+        for(let exportItem in exportData["drawflow"]){
+            if(exportItem != "dummy" && exportItem != "Home"){
+                processData[exportItem.replace('agent-','')] = exportData["drawflow"][exportItem]
+                this.canvas.removeModule(exportItem)
+            }
+        }
+        $('#model-process-agent-types-select-button').text("Select Agent type to model agent process")
+        return processData
     }
 
-    __flowyOnGrab(block){
-        // When the user grabs a block
+    changeCanvasModule(triggerElement){
+        $('#model-process-agent-types-select-button').text($(triggerElement).text())
+        $(triggerElement).parent().parent().find("a").removeClass("active")
+        $(triggerElement).addClass("active")
+        this.canvas.changeModule("agent-"+$(triggerElement).attr("agent-type-id"))
     }
 
-    __flowyOnRelease(){
-        // When the user releases a block
+    addProcessStepElement(){
+        this.canvas.addNode('block', 1, 1, 300, 300, 'start-element', {}, $("#agent-process-block-element").html())
     }
 
-    __flowyOnSnap(block, first, parent){
-        // When a block snaps with another one
-        return true
-    }
-
-    __flowyOnRearrange(block, parent){
-        // When a block is rearranged
+    updateBlockCustomCode(){
+        this.canvas.updateNodeDataFromId(0, {"stepcode": "T"})
     }
 }
 
